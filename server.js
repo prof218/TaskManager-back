e66ea5143a8f71ae970eae3c0916538b6e1db1df
ronak-shreevya <ronak.patel@shreevya.com>
2025-11-12 14:27:00 +0530

    server changes

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 5000;
 const MONGO = process.env.MONGO_URI || 'mongodb://localhost:27017/taskmanager';
 
 app.use(express.json());
-app.use(cors({ origin: process.env.CLIENT_URL || 'http://localhost:5173' }));
+app.use(cors({ origin: process.env.CLIENT_URL || 'https://task-tab.netlify.app/' }));
 
 app.use('/api/auth', authRoutes);
 app.use('/api/tasks', taskRoutes);
